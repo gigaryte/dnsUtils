9c0938d75e99bc1ff200d233ebea8ed5bd2389f3
erik <erik@fake.notreal>
2022-02-05 20:12:53 -0500

    initial commit

---
+++ dnsUtils.go
@@ -0,0 +1,24 @@
+package dnsutils
+
+import (
+	au "github.com/gigaryte/addrUtils"
+	"github.com/miekg/dns"
+)
+
+const (
+	TypeNet uint16 = 1024
+)
+
+// Net RR (this is a fake construct to return unique IPv6 address each time)
+type Net struct {
+	Hdr    dns.RR_Header
+	V6addr *au.IPv6Addr
+	Mask   uint8
+}
+
+func (rr *Net) String() string {
+	return rr.Hdr.String()
+}
+
+func (rr *Net) Next() {
+}
