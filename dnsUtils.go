09752bfc429aa02d8405346a40b0016ed5aa1075
erik <erik@fake.notreal>
2022-02-05 21:57:34 -0500

    debugging in import

--- dnsUtils.go
+++ dnsUtils.go
@@ -12,9 +12,12 @@ const (
 
 // Net RR (this is a fake construct to return unique IPv6 address each time)
 type Net struct {
-	Hdr    dns.RR_Header
-	V6addr *au.IPv6Addr
-	Mask   uint8
+	Hdr   dns.RR_Header
+	V6Net au.IPv6Network `dns:"net"`
+}
+
+func (rr *Net) Header() *dns.RR_Header {
+	return &rr.Hdr
 }
 
 func (rr *Net) String() string {
@@ -22,4 +25,21 @@ func (rr *Net) String() string {
 }
 
 func (rr *Net) Next() {
+}
+
+func (rr *Net) Len() {
+}
+
+func (rr *Net) Copy() {
+}
+
+func (rr *Net) Pack() {
+
+}
+func (rr *Net) Unpack() {
+}
+
+func (rr *Net) Parse() {
+}
+func (rr *Net) isDuplicate() {
 }
